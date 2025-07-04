097a1995f08dbe94d8e6316ff45ad95d557ae6c9
Hafiz Muhammad Talal <muhammadtala20201@gmail.com>
2025-07-05 01:44:03 +0500

    Create Tutorial_1.py

---
+++ Tutorial_1.py
@@ -0,0 +1,85 @@
+'''this is my first python program
+ This is a 
+ multi line comment
+
+print('Hello World')
+
+a = 5
+print(type(a))
+b = 5.5
+print(type(a))
+c =  3 + 4j #a + bj
+print(type(c))
+
+d = """string1"""
+print(type(d))
+
+a = 5
+b = 6.5
+c = 3 + 5j
+d = a + b + c
+print(d)
+print(type(d))
+
+a = 5 + 6j
+b = 'a'
+c = str(a) + b
+print(c)
+
+a, b, c = 10, 20, 30
+d = e = 'abc'
+print(d,e)
+
+a = float(input("Enter something: "))
+print(a)
+print(type(a))
+
+print('this is the value of', a)
+
+print(f'This is the value of {a}')  #f'strings
+
+ &&, ||, ! in C, C++
+and, or, not in python
+
+a = 5
+b = 10
+
+print(not a > b) #True or False
+
+
+if-else 
+a = (input("Enter a number: "))
+ if a > 1:
+     print(a)
+is, in
+ if a > 5:
+     print("true")
+ else:
+     print("false")
+
+ if a in range(1, 11):
+     print("Exist")
+ else:
+     print("Donot exist")    
+    
+ if a is True:
+     print("TRUE")
+if, else if
+a = 10
+ if a > 10:
+     print("Large")
+ elif a < 10:
+     print("Small")
+ else:
+     print("a = 10")    
+ a = 10
+ if a > 5:
+     if a > 3:
+         print("True")
+     else:
+         print("False")    
+        
+is -> ==
+ a, b = 5, 5
+ print(a is b)
+ '''
