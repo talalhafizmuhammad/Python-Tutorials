1e963ee9e9abfb6e26be524877456d5cf32945b8
Hafiz Muhammad Talal <muhammadtala20201@gmail.com>
2025-07-26 02:04:29 +0500

    Update AdvTutorial4.py

--- AdvTutorial4.py
+++ AdvTutorial4.py
@@ -1,126 +1,127 @@
-# math -> Includes advance math functions and constants
+'''math -> Includes advance math functions and constants
 
 import math
 
-#Square root -> sqrt()
+Square root -> sqrt()
 
-# a = math.sqrt(10)
-# print(a)
+a = math.sqrt(10)
+print(a)
 
-#Constant PI
+Constant PI
 Pi = math.pi
 
 
-#Sine function
+Sine function
 
-# a = math.sin(Pi/2)
+a = math.sin(Pi/2)
 
-#Cosine function
+Cosine function
 
-# b = math.cos(0)
+b = math.cos(0)
 
-#Tangent function
+Tangent function
 
-# c = math.tan(Pi/4)
-
-
-# print(a, b, c)
+c = math.tan(Pi/4)
 
 
-#Exponential function
+print(a, b, c)
 
-#e^2 (gives the value of const(e) raise to the power)
 
-# a = math.exp(2)
-# print(a)
+Exponential function
 
-#Ceiling and flooring
+e^2 (gives the value of const(e) raise to the power)
 
-# a = math.ceil(3.01)
-# b = math.floor(3.99)
+a = math.exp(2)
+print(a)
 
-# print(a, b)
+Ceiling and flooring
 
-#Program to calculate area of circle
+a = math.ceil(3.01)
+b = math.floor(3.99)
 
-# radius = float(input("Enter radius: "))
+print(a, b)
 
-# area = Pi * radius ** 2
+Program to calculate area of circle
 
-# print(f"Area of circle with radius {radius} is {area}")
+radius = float(input("Enter radius: "))
 
-#timeit Module -> Measures execution time
+area = Pi * radius ** 2
 
-# import timeit
+print(f"Area of circle with radius {radius} is {area}")
 
-#Usage
+timeit Module -> Measures execution time
 
-# time = timeit.timeit("print('TEST')", number=1000)
-# print(f"Execution time is: {time}")
-# x = 0
+import timeit
 
-# time = timeit.timeit("for i in range(100): pass", number = 1000)
-# print(time)
+Usage
 
-#Calendar Module -> Handle calendar related functions
+time = timeit.timeit("print('TEST')", number=1000)
+print(f"Execution time is: {time}")
+x = 0
+
+time = timeit.timeit("for i in range(100): pass", number = 1000)
+print(time)
+
+Calendar Module -> Handle calendar related functions
 
 import calendar
 
-#Displaying calendar of a month of any year
+Displaying calendar of a month of any year
 
-# cal = calendar.month(2029, 8)
-# print(cal)
+cal = calendar.month(2029, 8)
+print(cal)
 
-#print month names
+print month names
 
-# for i in range(1, 13):
-    # print(calendar.month_name[i])
+for i in range(1, 13):
+    print(calendar.month_name[i])
     
-#Check if it is a leap year or not
+Check if it is a leap year or not
 
-# YEAR = int(input("Enter an year: "))
-# print(f"Year is leap? {calendar.isleap(YEAR)}")
+YEAR = int(input("Enter an year: "))
+print(f"Year is leap? {calendar.isleap(YEAR)}")
 
-#Day of the week for a date
+Day of the week for a date
 
-# day = calendar.weekday(2025, 7, 25)
+day = calendar.weekday(2025, 7, 25)
 
-# print(day)  #0 -> Monday....onwards
+print(day)  #0 -> Monday....onwards
 
 
-#To print calendar of an year
-# YEAR = int(input("Enter an year: "))
+To print calendar of an year
+YEAR = int(input("Enter an year: "))
 
-# for month in range(1, 13):
-    # print(calendar.month(YEAR, month))
-    # print()
+for month in range(1, 13):
+    print(calendar.month(YEAR, month))
+    print()
     
-#Count how many friday 13th occur in given year
+Count how many friday 13th occur in given year
 
-# YEAR = int(input("Enter an year: "))
+YEAR = int(input("Enter an year: "))
 
-# count = 0
+count = 0
 
-# for month in range(1, 13):
-#     if calendar.weekday(YEAR, month, 13) == 4:
-#         count += 1
-# print(count)
+for month in range(1, 13):
+    if calendar.weekday(YEAR, month, 13) == 4:
+        count += 1
+print(count)
 
-# FUNCTOOLS module -> Offers higher-order functions that acts on the function, or on some other functions
+FUNCTOOLS module -> Offers higher-order functions that acts on the function, or on some other functions
 
-# -> lru_cache -> usage of lru_cache is called memoization (Decreases complexity)
+-> lru_cache -> usage of lru_cache is called memoization (Decreases complexity)
 
-# fibbonacci series -> 0, 1, 1, 2, 3, 5, 8, 13, 21, ....
+fibbonacci series -> 0, 1, 1, 2, 3, 5, 8, 13, 21, ....
 
-# Time complexity of recursion -> O(n^2)
+Time complexity of recursion -> O(n^2)
 
-# import functools
-# from functools import lru_cache
+import functools
+from functools import lru_cache
 
-# @lru_cache(maxsize=None)
-# def fab(n):
-#     if n <= 1:
-#         return n
-#     else:
-#         return fab(n-1) + fab(n-2)
-# print(fab(77))
+@lru_cache(maxsize=None)
+def fab(n):
+    if n <= 1:
+        return n
+    else:
+        return fab(n-1) + fab(n-2)
+print(fab(77))
+'''
