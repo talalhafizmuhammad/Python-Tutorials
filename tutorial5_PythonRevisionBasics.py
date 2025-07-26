1badd3ecedaed0880f9a0ac114ad19e92f1a9aa9
Hafiz Muhammad Talal <muhammadtala20201@gmail.com>
2025-07-27 04:14:01 +0500

    Update and rename tutorial5.py to tutorial5_PythonRevisionBasics.py

--- tutorial5_PythonRevisionBasics.py
+++ tutorial5_PythonRevisionBasics.py
@@ -1,7 +1,13 @@
-'''name = 'Ali'  #string 
-age = 20  #integer
-pi = 3.14  #float
-is_student = True  #boolean
+"""
+Topic: Python Basics Revision
+Covers: Variables, Data Types, Type Casting, Conditions, Loops, Match-Case
+"""
+
+# Variables and Data Types
+name = 'Ali'         # string
+age = 20             # integer
+pi = 3.14            # float
+is_student = True    # boolean
 
 print(type(name))
 print(type(age))
@@ -10,44 +16,36 @@ print(type(is_student))
 
 print(name, age, pi, is_student)
 
-Task input
-
-a = input("Enter a string: ")  #string
+# Input and Type Checking
+a = input("Enter a string: ")
 print(a, type(a))
 
 a = int(input("Enter an integer: "))
 print(a, type(a))
 
-Type Casting
-
+# Type Casting
 a = input("Enter: ")
 print(a, type(a))
-
 a = int(a)
 print(a, type(a))
 
+# Sum of two numbers
 a = int(input("Enter 1st number: "))
-b = int(input('Enter 2nd number: '))
-
+b = int(input("Enter 2nd number: "))
 print(f"Sum is {a + b}")
 
-Conditions: if, elif, else, nested if
-
-is positive or negative or 0
-
+# Conditions: if, elif, else
+# Check if a number is positive, negative or zero
 num = int(input("Enter a number: "))
-
 if num > 0:
-    print('Positive')
+    print("Positive")
 elif num < 0:
-    print('Negative')
+    print("Negative")
 else:
     print("Zero")
 
-check whether a number is divisible by 3 or 5 or both
-
+# Check if a number is divisible by 3 or 5 or both
 num = int(input("Enter a number: "))
-
 if num % 3 == 0 and num % 5 == 0:
     print(f"{num} is divisible by both 3 and 5")
 elif num % 3 == 0:
@@ -57,45 +55,29 @@ elif num % 5 == 0:
 else:
     print(f"{num} is not divisible")
 
-Match case
-
-input in value
-
-match value:
-    case 1:
-        print(lines)
-    case 2:
-        print(lines)
-    case _:
-        print(unknown)
+# Match-case statement (Python 3.10+)
 num = int(input("Enter a number: "))
 match (num % 3 == 0, num % 5 == 0):
     case (True, True):
         print("Both divisible")
     case (True, False):
-        print("Div by 3")
+        print("Divisible by 3 only")
     case (False, True):
-        print('Div by 5')
-    case (False, False):
-        print('Not div')
+        print("Divisible by 5 only")
+    case _:
+        print("Not divisible by 3 or 5")
 
-while condition:
-    code
-    inc/dec
-
+# While loop
 i = 1
 while i <= 5:
     print(i)
     i += 1
 
-for i in range(start, end):
-    code
-
+# For loop
 for i in range(1, 11):
     print(i)
 
-Table of 5
+# Multiplication Table of 5
 a = 5
 for i in range(1, 11):
-    print(f"{a} * {i} = {a*i}")
-    '''
+    print(f"{a} * {i} = {a * i}")
